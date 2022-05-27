6ff409992ea1e0ff28cf80ac54925c08b5667ce4
pattontj <60120527+pattontj@users.noreply.github.com>
2022-05-26 20:41:16 -0700

    Timeouts actually work now, untracked a bunch of useless files

--- Nanashi/Program.cs
+++ Nanashi/Program.cs
@@ -11,6 +11,11 @@ using Discord.WebSocket;
 using System.Xml;
 using System.Xml.Serialization;
 using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.Timers;
+using System.Threading;
+using Microsoft.VisualBasic;
+using Discord.Rest;
 
 namespace Nanashi
 {
@@ -41,10 +46,23 @@ namespace Nanashi
             {"simp", "chad" },
             };
 
+        // TODO: load this from a file or something
         private readonly List<ulong> Moderators = new List<ulong>
         {
 
         };
+
+
+        // only hold users in runtime memory (no logging)
+        //                          msgID, userID
+        private readonly Dictionary<ulong, ulong> posters = new Dictionary<ulong, ulong> {
+            
+        };
+
+
+        private readonly Dictionary<ulong, System.Timers.Timer> Timeouts = new Dictionary<ulong, System.Timers.Timer> { };
+        private readonly List<ulong> Bans = new List<ulong> { };
+
 
         public static void Main()
          => new Program().MainAsync().GetAwaiter().GetResult();
@@ -74,10 +92,11 @@ namespace Nanashi
 
             String token;
 
-            using (StreamReader sr = File.OpenText("Token.txt")) {
+            using (StreamReader sr = File.OpenText("Token.txt"))
+            {
                 string tk = sr.ReadLine();
 
-                
+
 
                 if (tk == null)
                     throw new NotImplementedException("Token not found");
@@ -88,7 +107,7 @@ namespace Nanashi
                 sr.Close();
             }
 
-                client = new DiscordSocketClient();
+            client = new DiscordSocketClient();
 
             await client.LoginAsync(TokenType.Bot,
                 token,
@@ -110,7 +129,7 @@ namespace Nanashi
             if (msg.Author.Id != client.CurrentUser.Id)
             {
 
-                if (msg.Content[0] == '!') 
+                if (msg.Content[0] == '!')
                 {
                     HandleModeratorAction(msg);
                     return;
@@ -118,10 +137,16 @@ namespace Nanashi
 
                 if (msg.Channel.GetType().Equals(typeof(SocketDMChannel)))
                 {
+
+                    if (Bans.Contains(msg.Author.Id) || Timeouts.ContainsKey(msg.Author.Id))
+                    {
+                        return;
+                    }
+
+
                     globalPostCount += 1;
                     // await msg.Channel.SendMessageAsync("Got DM");
                     // Console.WriteLine("Post No. " + globalPostCount + ", user: " + msg.Author.Username);
-
 
 
                     XmlSerializer Serializer = new XmlSerializer(typeof(Post));
@@ -139,6 +164,7 @@ namespace Nanashi
                     ns
                     );
                     string xmlResult = sww.GetStringBuilder().ToString().Replace("<? xml version = \"1.0\" encoding = \"utf-16\"?>", " ");
+
 
 
                     File.AppendAllText("posts.db", xmlResult);
@@ -175,7 +201,7 @@ namespace Nanashi
                                     {
                                         var begin = lowerMsg.IndexOf(kvp.Key);
                                         var end = lowerMsg.LastIndexOf(kvp.Key);
-                                        message = message.Substring(0, begin) + kvp.Value + message.Substring(end+kvp.Key.Length);
+                                        message = message.Substring(0, begin) + kvp.Value + message.Substring(end + kvp.Key.Length);
                                     }
                                     // message = message.Replace(kvp.Key, kvp.Value);
                                 }
@@ -196,7 +222,9 @@ namespace Nanashi
                                     sw.Close();
                                 }
 
-                                await chan.SendMessageAsync(null, false, embed.Build(), null);
+                                var sentMsg = await chan.SendMessageAsync(null, false, embed.Build(), null);
+
+                                posters.Add(sentMsg.Id, msg.Author.Id);
                             }
                         }
                     }
@@ -204,38 +232,152 @@ namespace Nanashi
             }
         }
 
-        public void HandleModeratorAction(SocketMessage msg) 
+        public void HandleModeratorAction(SocketMessage msg)
         {
+            Regex timeExpression = new Regex(@"^(?:\d+(?::[0-5][0-9]:[0-5][0-9])?|[0-5]?[0-9]:[0-5][0-9])$");
 
-            Regex timeExpression = new Regex(@"(\d{0,}d)(\d{0,}h)(\d{0,}m)*");
+            if ( !Moderators.Contains(msg.Author.Id) )
+            {
+                return;
+            }
 
-          
-
-            //   " /(ban\s)\d{1,}\g "
-
-            if (msg.Content.Contains("ban")) {
+            if (msg.Content.Contains("ban"))
+            {
                 Console.WriteLine("ban");
+                BanUser(0);
             }
 
             //  Timeout regex    " /(\d{0,}d)*:(\d{0,}h)*:(\d{0,}m)*/g  "
 
-            if (msg.Content.Contains("timeout")) {
-                var matches = timeExpression.Match(msg.Content);
-                Console.WriteLine("timeout test");
+            if (msg.Content.Contains("timeout"))
+            {
+                string[] cmdArgs = msg.Content.Split(' ');
 
-                
-                var timelist = Regex.Split(msg.Content, @"\D+");
-
-                foreach (var t in timelist)
+                if (cmdArgs.Length != 3)
                 {
-                    Console.WriteLine(t);
+                    msg.Channel.SendMessageAsync("command failed, must use format [postID] [hh:mm:ss]");
+                    return;
                 }
 
-              
+                var msgID = ulong.Parse(cmdArgs[1]);
+                string timestamp = cmdArgs[2];
+                Console.WriteLine(msgID);
+                Console.WriteLine(timestamp);
 
+                ulong pstr;
+                try
+                {
+                    pstr = posters[msgID];
+                }
+                catch
+                {
+                    msg.Channel.SendMessageAsync("Poster not found");
+                    return;
+                }
+
+
+                var timelist = Array.ConvertAll<string, int>(
+                        Regex.Split(timestamp, @"\D+"),
+                        item => Int32.Parse(item)
+                    );
+
+                var timeSeconds = 0;
+
+                for (int i = 0, e = 3600; i < timelist.Length; i++)
+                {
+                    timeSeconds += timelist[i] * e;
+                    e /= 60;
+                }
+
+                var timeoutTimer = new System.Timers.Timer(timeSeconds*1000);
+                timeoutTimer.Elapsed += new System.Timers.ElapsedEventHandler(
+                    (object source, ElapsedEventArgs e) =>
+                    {
+                        timeoutTimer.Stop();
+                        Timeouts.Remove(pstr);
+                    });
+                timeoutTimer.Enabled = true;
+
+                Timeouts.Add(pstr, timeoutTimer);
+                msg.Channel.SendMessageAsync("message ID " + msgID + " has been timed out for " + timeSeconds + " seconds");
+
+                EditMessageWithTimeout(msgID, timestamp);
 
             }
         }
 
+
+        async public void EditMessageWithTimeout(ulong postID, string timestamp)
+        {
+            foreach (var guild in client.Guilds)
+            {
+                foreach (var channel in guild.Channels)
+                {
+                    var chan = channel as ITextChannel;
+
+                    if (chan?.Name == "anonymous")
+                    {
+                        var msg = await chan.GetMessageAsync(id: postID);
+
+                        var iMsg = (RestUserMessage)msg;
+
+                        if (msg.Author.Id == client.CurrentUser.Id)
+                        {
+                            var embed = new EmbedBuilder();
+
+                            embed.WithDescription(msg.Embeds.First().Description);
+                            if (msg.Embeds.First().Fields.Length != 0)
+                            {
+                                var nam = msg.Embeds.First().Fields.First().Name;
+                                nam = (nam == null) ? nam : "Punished Guca";
+                                var val = msg.Embeds.First().Fields.First().Value;
+                                val = (val == null) ? val : "Press F";
+
+                                embed.AddField(nam, val, false);
+                            }
+                            embed.AddField("USER WAS TIMED OUT FOR THIS POST", timestamp, false);
+
+                            await iMsg.ModifyAsync(m => { m.Embed = embed.Build(); });
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+
+        public void BanUser(int postCode)
+        {
+            // XML stuff here
+            Console.WriteLine("test");
+
+            XmlDocument posts = new XmlDocument();
+            posts.Load("posts.db");
+
+            XmlNode root = posts.ChildNodes[0];
+
+            Console.WriteLine("test 2");
+
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine(post);
+            }
+
+        }
+
+        public void SerializePost(Post post)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("posts.db");
+
+            XmlNode root = doc.ChildNodes[0];
+
+        }
+
     }
 }
+
+
